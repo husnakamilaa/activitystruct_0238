85891b1cb455f3a3769a8d9262c6b6c6a5977194
husnakamilaa <husna.kamila.ft23@mail.umy.ac.id>
2024-04-02 14:03:52 +0700

    membuat structure Mahasiswa

--- activitystruct_0238.cpp
+++ activitystruct_0238.cpp
@@ -4,6 +4,13 @@
 #include <iostream>
 using namespace std;
 
+struct Mahasiswa {
+    string nim;
+    string nama;
+    string alamat;
+    string umur;
+};
+
 int main()
 {
     std::cout << "Hello World!\n";
