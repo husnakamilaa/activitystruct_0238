080dc585fe1bc9ceb21d5ab95a615be9f090c745
husnakamilaa <husna.kamila.ft23@mail.umy.ac.id>
2024-04-02 15:47:13 +0700

    membuat input data untuk nim, nama, alamat, dan umur pada contoh 3

--- activitystruct3_0238/activitystruct3_0238.cpp
+++ activitystruct3_0238/activitystruct3_0238.cpp
@@ -18,5 +18,18 @@ struct Mahasiswa {
 
 int main(){
     Mahasiswa mhs;
+    cout << "Nomor Mahasiswa : ";
+    getline(cin , mhs.nim);
+    cout << "Nama Mahasiswa : ";
+    getline(cin, mhs.nama);
+
+    cout << "Alamat Mahasiswa : " << endl;
+    cout << "\t Nama Desa : ";
+    cin >> mhs.alamat.desa;
+    cout << "\t Nama Kota : ";
+    cin >> mhs.alamat.kota;
+
+    cout << "Umur Mahasiswa : ";
+    cin >> mhs.umur;
     
 }
