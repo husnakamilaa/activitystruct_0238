a8ed25d662e71bcd33aaad76437cf9db8ee8d2d1
husnakamilaa <husna.kamila.ft23@mail.umy.ac.id>
2024-04-02 15:31:35 +0700

    membuat structure DetailAlamat pada contoh 3

---
+++ activitystruct3_0238/activitystruct3_0238.cpp
@@ -0,0 +1,15 @@
+// activitystruct3_0238.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+
+struct DetailAlamat {
+    string desa;
+    string kota;
+};
+
+int main()
+{
+    
+}
